9146b20169107f4b60ccb42f63a331fa5e77583c
Mirismoil <yuldashevmirismoil3@gmail.com>
2022-08-06 19:15:54 +0500

    footer-commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+
+var elModebtn = document.querySelector(".burger")
+var elbody = document.querySelector("body")
+
+elModebtn.addEventListener("click", function(){
+    elbody.classList.toggle("dark")
+})
